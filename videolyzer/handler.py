a0187016806e0e91ff6b92e39cf7a9fa45826974
Yang Dai <yangdai713@aim.com>
2018-09-10 17:17:40 -0400

    publish job completion to SNS topic, and handle via lambda function

--- videolyzer/handler.py
+++ videolyzer/handler.py
@@ -2,6 +2,8 @@ from urllib.parse import unquote_plus
 
 import boto3
 
+import os
+
 
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
@@ -11,6 +13,10 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket,
                 'Name': key,
             }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN'],
         }
     )
 
@@ -25,5 +31,12 @@ def start_processing_video(event, context):
             record['s3']['bucket']['name'],
             unquote_plus(record['s3']['object']['key'])
         )
+
+    return
+
+
+def handle_label_detection(event, context):
+
+    print(event)
 
     return
